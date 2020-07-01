c8ee137a2a36dcf61ada999051fd66d4064c5b80
kovacevicemir <emirkovacevic92@gmail.com>
2020-07-01 19:00:18 +1000

    adding express

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -2,56 +2,53 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 
 admin.initializeApp()
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Helloooo world");
-});
 
-exports.getScreams = functions.https.onRequest(async (req, res) => {
-    const data = await admin.firestore().collection('screams').get()
+const express = require('express')
+const app = express()
+
+
+app.get('/screams', async (req,res)=>{
+    
+    const data = await admin.firestore()
+                            .collection('screams')
+                            .orderBy('createdAt', 'desc')
+                            .get()
 
     let screams = []
     data.forEach(scream => {
-        screams.push(scream.data())
+        screams.push({
+            screamId: scream.id,
+            body: scream.data().body,
+            userHandle: scream.data().userHandle,
+            createdAt: scream.data().createdAt
+        })
     })
-
-    // admin.firestore().collection('screams').get()
-    // .then(data => {
-    //     let screams = []
-    //     data.forEach(doc => {
-    //         screams.push(doc.data())
-    //     })
-
-    //     return res.json(screams)
-    // }).catch(err => console.error(err))
-
+    
     res.json(screams)
 })
 
-exports.createScream = functions.https.onRequest(async (req, res) => {
 
-    if(req.method !== 'POST'){
-        return res.status(400).json({error: 'Method not allowed'})
-    }
+app.post('/scream', async (req, res) => {
 
     try {
         const newScream = {
             body: req.body.body,
             userHandle: req.body.userHandle,
-            createdAt: admin.firestore.Timestamp.fromDate(new Date())
+            createdAt: new Date().toISOString()
         }
     
         const docRef = await admin.firestore()
             .collection('screams')
             .add(newScream)
 
-        res.json({message: `document ${docRef.doc.id} created successfully`})
+        res.json({message: `document created successfully`})
 
     } catch (err) {
         console.error(err)
-        res.status(500).json({error: 'something went wrong'})
+        res.status(500).json({error: `something went wrong: ${err}`})
     }
 
 })
+
+//https://baseurl.com/api/
+exports.api = functions.https.onRequest(app)
